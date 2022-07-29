293ce6a425bb7bc01901e8e3ab5b83d563caea70
JaredHerrera6 <herrerajared13@gmail.com>
2022-07-29 18:58:31 -0400

    Changing flex direction for mobile

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -34,6 +34,7 @@ function Footer() {
                             <li className='footer-header'>Contact Information</li>
                             <li>Based In Prince George's County</li>
                             <li>240-602-8980</li>
+                            <li>thejavismp@gmail.com</li>
                         </ul>
                     </div>
                 </div>
